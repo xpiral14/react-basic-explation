51cdcc67101542bce1d63239d86b71c57de6f66b
Samuel Reis <sa.reis1999@gmail.com>
2020-10-31 13:03:46 -0300

    Explanation about CSS-IN-JS

--- src/components/BookInfo/styles.ts
+++ src/components/BookInfo/styles.ts
@@ -1,5 +1,7 @@
 import styled from "styled-components";
-
+/**
+ * Styled-Components é uma biblioteca capaz de gerar CSS-IN-JS (pesquisar mais sobre), isso dá um ganho enorme em questão de themificação da aplicação pois aqui podemos criar lógica no css de acordo com o que quisermos
+ */
 export const Container = styled.div`
   border: 2px solid black;
   margin-bottom: 30px;
