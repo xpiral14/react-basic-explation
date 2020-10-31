af0737182e08f7c1c393a201572ece5ad1e425ee
Samuel Reis <sa.reis1999@gmail.com>
2020-10-31 13:03:25 -0300

    Explanation about components

--- src/components/BookInfo/index.tsx
+++ src/components/BookInfo/index.tsx
@@ -2,8 +2,11 @@ import React from "react";
 import Book from "../../contracts/models/Book";
 import { Container } from "./styles";
 
-// import { Container } from './styles';
-
+/**
+ * Um componentes são partes do sistema que você sabe que terá reutilização. Nele você é capaz de passar propriedades que definirão o conteúdo, lógica e ação do mesmo. Esses atributos aceitam qualquer coisa que você quiser atribuir a eles desde que seja válido para o Javascript.
+ * 
+ * Nesse caso criamos um componente de informações de um livro. Como eu sei que teremos que mostrar vários livros, a melhor coisa é extrair essa lógica para um componente, pois assim o código fica mais enxuto, encapsulado e fácil de enteder.
+ */
 const BookInfo: React.FC<Book> = (props) => {
   return (
     <Container>
